86594e58081396ba5a593bc893fc167e65925c40
alicoder123411 <sameersingla240@gmail.com>
2026-01-30 14:21:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+const form = document.getElementById("loginForm");
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const existingBtn = document.getElementById("existing");
+if (localStorage.getItem("username")) {
+  existingBtn.style.display = "block";
+}
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const username = usernameInput.value;
+  const password = passwordInput.value;
+  alert(`Logged in as ${username}`);
+  if (checkbox.checked) {
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+    existingBtn.style.display = "block";
+  } else {
+    localStorage.clear();
+    existingBtn.style.display = "none";
+  }
+});
+existingBtn.addEventListener("click", function () {
+  const savedUser = localStorage.getItem("username");
+  if (savedUser) {
+    alert(`Logged in as ${savedUser}`);
+  }
+});
